1a14722be3b980119b07e68e814cd75f72198a40
rajosch <raphaeljschmid@protonmail.com>
2023-03-22 12:18:33 -0600

    commit

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-## Setup
-
-Make sure to install the dependencies:
-
-```bash
-node setup_tailwind.js
-```
-
 ## Development Server
 
 Start the development server on http://localhost:3000
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,3 @@
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',    
-    ],
-        tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        configPath: 'tailwind.config.js',
-        exposeConfig: false,
-        injectPosition: 0,
-        viewer: true,
-    },
 })
         
